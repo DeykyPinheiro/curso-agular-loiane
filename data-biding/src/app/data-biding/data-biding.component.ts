1f008bf9e326c2e88c08ff466358d519840f2d8e
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-11-06 18:13:04 -0300

    event biding

--- data-biding/src/app/data-biding/data-biding.component.ts
+++ data-biding/src/app/data-biding/data-biding.component.ts
@@ -13,12 +13,41 @@ export class DataBidingComponent {
 
   cursoAngular: boolean = true;
 
+  textDigitado: string = "";
+
+  textoSalvo: string = "";
+
+  isMouseOver: boolean = false;
+
+  // usado no property biding
   getValor(): number {
     return 10;
   }
 
+
+
+  // usado no property biding
   getCurtirCurso(): boolean {
     return this.cursoAngular;
+  }
+
+  // escutar evento do botao
+  botaoClicado(): void {
+    alert("clcaram em mim");
+  }
+
+  onkeyUp(event: KeyboardEvent): void {
+    this.textDigitado = (<HTMLInputElement>event.target).value;
+    // alert("texto digitado: " +  this.textDigitado)
+  }
+
+  pressEnter(event: any): void {
+    this.textoSalvo = event;
+    this.textoSalvo = (<HTMLInputElement>event.target).value;
+  }
+
+  mouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
 
